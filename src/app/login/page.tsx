413ba89267b185120b4f8cad2ae0895426b72ebd
Sourav Ganguly <isouravganguly@gmail.com>
2024-04-21 22:43:05 +0530

    add: initial_commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
 export default function loginPage() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>Login Page</h1>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Login Page</h1>
+          <h2>This is a change by Sourav Ganguly</h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
