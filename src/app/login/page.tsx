85e720815ba46132e186109774e0e678cd49d068
Sourav Pradhan <sourav204@gmail.com>
2024-04-21 22:29:09 +0530

    changes made

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function loginPage() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Login Page</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
